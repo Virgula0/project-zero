0d1f2ba756f4ed3ed3ea00a739d979fe035cc33b
Virgula <ngelo.rosa@protonmail.com>
2025-03-28 15:50:19 +0100

    adding loading scene animation

---
+++ Assets/Scripts/Loading/Loading.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Loading : MonoBehaviour
+{
+    private RectTransform rectComponent;
+    private Image imageComp;
+    private bool up;
+
+    public float rotateSpeed = 200f;
+    public float openSpeed = .005f;
+    public float closeSpeed = .01f;
+    private const float MaxFillThreshold = 0.30f;
+    private const float MinFillThreshold = 0.02f;
+    private bool increasing = true;
+
+    private void Start()
+    {
+        rectComponent = GetComponent<RectTransform>();
+        imageComp = rectComponent.GetComponent<Image>();
+        up = true;
+    }
+
+    private void Update()
+    {
+        rectComponent.Rotate(0f, 0f, rotateSpeed * Time.deltaTime);
+        ChangeSize();
+    }
+    private void ChangeSize()
+    {
+        float currentFill = imageComp.fillAmount;
+
+        // If we are increasing and haven't reached the maximum fill, increase the fill amount.
+        if (increasing && currentFill < MaxFillThreshold)
+        {
+            imageComp.fillAmount += openSpeed;
+            return;
+        }
+        
+        // If increasing but the maximum threshold is reached, reverse direction.
+        if (increasing && currentFill >= MaxFillThreshold)
+        {
+            increasing = false;
+            return;
+        }
+        
+        // If we are decreasing and above the minimum fill, decrease the fill amount.
+        if (!increasing && currentFill > MinFillThreshold)
+        {
+            imageComp.fillAmount -= closeSpeed;
+            return;
+        }
+        
+        // If decreasing but the minimum threshold is reached, reverse direction.
+        if (!increasing && currentFill <= MinFillThreshold)
+        {
+            increasing = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Loading/loadingtext.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class loadingtext : MonoBehaviour {
+
+    private RectTransform rectComponent;
+    private Image imageComp;
+
+    public float speed = 200f;
+    public Text text;
+    public Text textNormal;
+    private const float FullFill = 1f;
+    private const float ResetFill = 0f;
+
+    // Use this for initialization
+    void Start () {
+        rectComponent = GetComponent<RectTransform>();
+        imageComp = rectComponent.GetComponent<Image>();
+        imageComp.fillAmount = 0.0f;
+    }
+        
+    void Update()
+    {
+        // Only update if the fill amount is not full
+        if (imageComp.fillAmount != FullFill)
+        {
+            // Increase fill amount based on deltaTime and speed
+            imageComp.fillAmount += Time.deltaTime * speed;
+            
+            // Convert fill amount to percentage
+            int fillPercentage = (int)(imageComp.fillAmount * 100);
+            
+            // Update the text based on the current fill percentage
+            if (fillPercentage > 0 && fillPercentage <= 33)
+            {
+                textNormal.text = "Loading...";
+            }
+            // Additional conditions for other ranges can be added here if needed
+
+            text.text = fillPercentage + "%";
+        }
+        else
+        {
+            // Reset fill amount and text when full
+            imageComp.fillAmount = ResetFill;
+            text.text = "0%";
+        }
+    }
+
+}
