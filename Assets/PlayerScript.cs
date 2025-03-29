a40224a4fa0399f83d86f9bcc573b9ec5d1f8a48
Tycho4311 <76877590+Tycho4311@users.noreply.github.com>
2025-03-29 15:02:02 +0100

    added a player placeholder sprite and a player movement logic, created a test scene, created an Art folder.

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.InputSystem.iOS;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D playerRb;
+
+    private Vector2 moveDirection;
+    private Vector2 mousePos;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //add something later
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookAt();
+        ProcessInputs();
+        Movement();
+    }
+
+    void FixedUpdate()
+    {
+        //best suited for physics operations
+    }
+
+    private void ProcessInputs(){
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    private void Movement(){
+        playerRb.linearVelocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+    private void LookAt(){
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.right = mousePos - new Vector2(transform.position.x, transform.position.y);
+        
+        /*var dir = Input.mousePosition - Camera.main.ScreenToWorldPoint(transform.position);
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);*/
+    }
+}
