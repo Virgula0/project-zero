6551456faee9a4b51d6982fe06b164358dfa67a5
Virgula <ngelo.rosa@protonmail.com>
2025-03-31 22:15:50 +0200

    adding start button

---
+++ Assets/MenuButtons.cs
@@ -0,0 +1,27 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButtons : MonoBehaviour
+{
+    [SerializeField] private AudioSource positiveClick;
+    [SerializeField] private AudioSource onHoverEffect;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        this.positiveClick = GetComponents<AudioSource>()[0];
+        this.onHoverEffect = GetComponents<AudioSource>()[1];
+    }
+
+    private void ClickSound(){
+        this.positiveClick.Play();
+    }
+
+    public void OnHoverEffect(){
+        this.onHoverEffect.Play();
+    }
+
+    public void ClickStart(){
+        ClickSound();
+    }
+}
