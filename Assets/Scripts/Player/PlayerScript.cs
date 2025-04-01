f655b056e7bb53e66b396e14729b7fe8ea8254bd
Tycho4311 <76877590+Tycho4311@users.noreply.github.com>
2025-04-01 12:45:43 +0200

    make player movement smooth

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -8,23 +8,21 @@ public class PlayerScript : MonoBehaviour
     private Vector2 moveDirection;
     private Vector2 mousePos;
 
+    private SpriteRenderer playerSprite;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //add something later
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        LookAt();
-        ProcessInputs();
-        Movement();
+        playerSprite = GetComponentInChildren<SpriteRenderer>();
     }
 
     void FixedUpdate()
     {
-        //best suited for physics operations
+        //best suited for physics operations, using along with interpolation as interpolate
+        LookAt();
+        ProcessInputs();
+        Movement();
     }
 
     private void ProcessInputs(){
@@ -35,16 +33,34 @@ public class PlayerScript : MonoBehaviour
     }
 
     private void Movement(){
-        float time = Time.deltaTime;
-        playerRb.linearVelocity = new Vector2(moveDirection.x * moveSpeed * time, moveDirection.y * moveSpeed * time);
+        playerRb.linearVelocity = new Vector2(moveDirection.x * moveSpeed * Time.deltaTime, moveDirection.y * moveSpeed * Time.deltaTime);
     }
 
     private void LookAt(){
-        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.right = mousePos - new Vector2(transform.position.x, transform.position.y);
-        
-        /*var dir = Input.mousePosition - Camera.main.ScreenToWorldPoint(transform.position);
-        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);*/
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 playerMouseDistance = mousePos - playerRb.position;
+
+        // Get half the width or height of the sprite (whichever is larger)
+        float safeDistance = Mathf.Max(playerSprite.bounds.extents.x, playerSprite.bounds.extents.y);
+
+        if (playerMouseDistance.magnitude < safeDistance)
+        {
+            return; // Stop execution if the mouse is too close
+        }
+
+        // Calculate the direction from the player/gun to the mouse
+        Vector2 direction = (mousePos - (Vector2)transform.position).normalized;
+
+        // Convert direction vector to angle (in degrees)
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        if (angle > 90 || angle < -90)
+            transform.localScale = new Vector2(2, -2); // Flip sprite
+        else
+            transform.localScale = new Vector2(2, 2); // Normal sprite
+
+        // Apply rotation (z-axis in 2D)
+        float rotationSpeed = 10f;
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, 0f, angle), Time.deltaTime * rotationSpeed);
     }
 }
