dca503a5b34585e4d182c023068921acf8ad8ee9
Virgula <ngelo.rosa@protonmail.com>
2025-04-07 18:01:22 +0200

    add start game when clicking on start in the main menu

--- Assets/Scripts/Loading/SwitchScene.cs
+++ Assets/Scripts/Loading/SwitchScene.cs
@@ -4,16 +4,13 @@ using UnityEngine.SceneManagement;
 
 public class SwitchScene : MonoBehaviour
 {
-    private int currentScene;
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private IEnumerator Start()
     {
-        this.currentScene = SceneManager.GetActiveScene().buildIndex;
         // wait 3 seconds and then switch to the next scene, just for testing
         // in the real game once you proceed to the next room you load another scene
         yield return new WaitForSeconds(3); 
 
-        SceneManager.LoadScene(++currentScene);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
     }
 }
