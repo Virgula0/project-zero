5c0c5d072dbb7d71eba330a47ad8780c4c87fb78
Tycho4311 <76877590+Tycho4311@users.noreply.github.com>
2025-04-10 16:25:58 +0200

    added first iteration of dash logic

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
@@ -6,27 +8,81 @@ public class PlayerScript : MonoBehaviour
     public Rigidbody2D playerRb;
     private Vector2 moveDirection;
     private SpriteRenderer playerSprite;
+    private bool canDash;
+    private bool canMove;
+    private float currentDashTime;
+    private Vector2 lastMoveDirection = Vector2.right; // Default direction
+
+
+    [SerializeField] float startDashTime = 0.1f;
+    [SerializeField] float dashSpeed = 100f;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //add something later
         playerSprite = GetComponentInChildren<SpriteRenderer>();
+        this.canDash = true;
+        this.canMove = true;
+    }
+
+    void Update()
+    {
+        if (canDash && Input.GetKeyDown(KeyCode.Space))
+        {
+            canDash = false;
+            canMove = false;
+            ProcessInputs();
+            StartCoroutine(Dash(moveDirection));
+        }
     }
 
     void FixedUpdate()
     {
         //best suited for physics operations, using along with interpolation as interpolate
         LookAt();
+        if(!canMove){
+            return;
+        }
         ProcessInputs();
         Movement();
+    }
+
+    IEnumerator Dash(Vector2 direction){
+        currentDashTime = startDashTime; // Reset the dash timer.
+
+        Debug.Log("Dashing");
+        Debug.Log(direction);
+
+        while (currentDashTime > 0f)
+        {
+            currentDashTime -= Time.deltaTime; // Lower the dash timer each frame.
+
+            playerRb.linearVelocity = direction * dashSpeed; // Dash in the direction that was held down.
+            // No need to multiply by Time.DeltaTime here, physics are already consistent across different FPS.
+
+            yield return null; // Returns out of the coroutine this frame so we don't hit an infinite loop.
+        }
+
+        playerRb.linearVelocity = new Vector2(0f, 0f); // Stop dashing.
+
+        canDash = true;
+        Debug.Log("YOU CAN DASH AGAIN");
+        canMove = true;
     }
 
     private void ProcessInputs(){
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
-        moveDirection = new Vector2(moveX, moveY).normalized;
+        Vector2 input = new Vector2(moveX, moveY);
+        moveDirection = input.normalized;
+
+        if (input != Vector2.zero)
+        {
+            lastMoveDirection = moveDirection;
+        }
     }
 
     private void Movement(){
