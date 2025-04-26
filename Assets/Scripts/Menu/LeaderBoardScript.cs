3b542649f6a76a946c8f9c5936bb2c94dd5ebd72
virgula <angelo.rosa@protonmail.com>
2025-04-26 12:04:11 +0200

    adding leaderboard

--- Assets/Scripts/Menu/LeaderBoardScript.cs
+++ Assets/Scripts/Menu/LeaderBoardScript.cs
@@ -1,9 +1,24 @@
 using UnityEngine;
-using TMPro;
 
 public class LeaderBoardScript : MonoBehaviour
 {
-    public void LoadLeaderBoard(){
-        
+    [SerializeField] private GameObject row;          // The row prefab
+    [SerializeField] private Transform contentParent; // Content object from the ScrollView
+
+    void Start()
+    {
+        foreach (Transform child in contentParent)
+        {
+            Destroy(child.gameObject);
+        }
+
+        // Create 10 mock rows
+        for (int i = 0; i < 20; i++)
+        {
+            GameObject newRow = Instantiate(row, contentParent); // Sets Content as parent
+            newRow.transform.localScale = Vector3.one; // Ensure scale is correct
+
+            // newRow.GetComponentInChildren<Text>().text = $"Player {i}";
+        }
     }
 }
