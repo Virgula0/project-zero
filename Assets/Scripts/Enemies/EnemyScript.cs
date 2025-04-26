31a57222c186e681b9f0eb5df7c4003badd16e9b
virgula <angelo.rosa@protonmail.com>
2025-04-26 16:30:55 +0200

    adding enemy script for changing sprite direction of enemies dynamically

---
+++ Assets/Scripts/Enemies/EnemyScript.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+// updates sprites based on the movement
+public class EnemyScript : MonoBehaviour
+{
+    private Rigidbody2D body;
+    private Vector2 lastPosition;
+
+    void Start()
+    {
+        body = GetComponentInChildren<Rigidbody2D>();
+        lastPosition = body.position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 currentPosition = body.position;
+        Vector2 movement = currentPosition - lastPosition;
+
+        if (movement.sqrMagnitude > 0.001f)
+        {
+            float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
+            SpriteRenderer sprite = GetComponentInChildren<SpriteRenderer>();
+            sprite.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+        }
+        lastPosition = currentPosition;
+    }
+}
