87d6abae8b6f22e2355e14d9f24fea103580dca4
virgula <angelo.rosa@protonmail.com>
2025-04-30 20:06:04 +0200

    restore old workflow file

--- Assets/Scripts/Enemies/Goon/GoonAI.cs
+++ Assets/Scripts/Enemies/Goon/GoonAI.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using System.Collections;
-using Unity.VisualScripting;
 
 public class AI : MonoBehaviour, IEnemy, IPoints
 {
